d82051ca64dbb31c786564c5bee3f3880cfbc350
Metin Çetin <metinc309@gmail.com>
2023-01-19 01:26:51 +0300

    Addded visualize command

---
+++ commands/visualize.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
+const ChessImageGenerator = require("chess-image-generator")
+
+let generator = new ChessImageGenerator({
+    size:256,
+    light: 'rgb(200, 200, 200)',
+    dark: '#333333',
+    style: 'merida',
+    flipped: true
+})
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("visualize")
+        .setDescription("Generates chess board visual with given FEN string ")    
+    .addStringOption(option => 
+        option.setName("fen")
+        .setDescription("FEN string to generate the board with")),
+    async execute(interaction){
+
+        let fen = interaction.options.getString("fen")
+        await generator.loadFEN(fen)
+        let buffer = await generator.generateBuffer()
+
+        await interaction.reply({files:[buffer]})
+    }
+}
